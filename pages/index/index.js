fa327115e1eb5cb0ecc594daacd5725c63f1a279
ataola <zjt613@gmail.com>
2020-05-25 22:09:30 +0800

    fix bug: get userinfo

--- pages/index/index.js
+++ pages/index/index.js
@@ -44,13 +44,6 @@ Page({
           userInfo: app.globalData.userInfo,
           hasUserInfo: true
         });
-      } else if (this.data.canIUse) {
-        app.userInfoReadyCallback = res => {
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          });
-        }
       } else {
         wx.getUserInfo({
           success: res => {
@@ -59,12 +52,12 @@ Page({
               userInfo: res.userInfo,
               hasUserInfo: true
             });
+            this.setData({
+              selectedImage: this.data.userInfo.avatarUrl
+            });
           }
         });
       }
-      this.setData({
-        selectedImage: this.data.userInfo.avatarUrl
-      });
     }
   },
   getLocalPhoto: function () {
@@ -159,9 +152,35 @@ Page({
       });
     }
   },
-  onLoad: function () {},
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
   getUserInfo: function (e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
